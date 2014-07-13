cb3c6bdf131c70bf1888830a22d212582b3eed83
xdaben <mastaman@gmail.com>
2014-07-12 18:06:58 -0600

    I broke git for a second there, not sure why...

--- src/main/java/com/xdaben/letsmodreboot/LetsModReboot.java
+++ src/main/java/com/xdaben/letsmodreboot/LetsModReboot.java
@@ -1,6 +1,29 @@
 package com.xdaben.letsmodreboot;
 
- 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "LetsModReboot", name = "Let's Mod Reboot", version = "1.7.10-1.0")
 public class LetsModReboot
 {
+    @Mod.Instance("LetsModReboot")
+    public static LetsModReboot instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
 }
